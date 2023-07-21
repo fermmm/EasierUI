dda34414f4ce53645e9e32c72202bd40e9e136bb
Fernando <fermmm@gmail.com>
2023-07-21 16:26:45 -0500

    Uploaded asset

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # EasierUI
-This Unity 3D asset is meant to simplify the UI development. Currently is not maintained but it should work, PRs are welcome.
+
+This an old Unity 3D asset is meant to simplify the UI development. Currently is not maintained but it should work, PRs are welcome.
+
+# ⚠ Important:
+
+You can add the Rect Transform Extended component to any UI GameObject to practice and understand what this tool can do, but don't reference it from code and don't use it in production, because it's slow.
+
+Yur final code should use the following methods that will be added to the RectTransform (extension methods):
+
+```csharp
+yourRectTransform.GetPosition()
+yourRectTransform.SetPosition()
+
+yourRectTransform.GetAnchorsPosition()
+yourRectTransform.SetAnchorsPosition()
+
+yourRectTransform.SetPositionX()
+yourRectTransform.SetPositionY()
+
+yourRectTransform.SetWidth()
+yourRectTransform.SetHeight()
+
+yourRectTransform.SetAnchorsPositionX()
+yourRectTransform.SetAnchorsPositionY()
+
+yourRectTransform.SetAnchorsWidth()
+yourRectTransform.SetAnchorsHeight()
+
+yourRectTransform.GetPivotPosition()
+yourRectTransform.SetPivotPosition()
+```
+
+Also 4 options are added to the Tools menu in the Unity UI:
+
+    - UI Anchors To Corners
+    - UI Corners To Anchors
+    - UI Center Rect
+    - UI Center Anchors
+
+# Usange example:
+
+```csharp
+RectTransfrom myRectTransform = transform as RectTransform;
+Vector2 size = myRectTransform.GetSize(CoordinateSystem.IgnoreAnchorsAndPivot);
+```
+
+If you need a more advanced usage converting coordinates instead of setting and getting, use the methods of the static classes:
+
+`RteRectTools` and `RteAnchorTools`
+Also these static classes contains some extra coordinate conversion methods.
---
+++ UI/RectTransform Extended/Core/RtePivotTools.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class RtePivotTools
+{
+    public static void SetPivotPosition(RectTransform transform, Vector2 newPivotPos, bool alsoMoveRect = false)
+    {
+        if(alsoMoveRect)
+        {
+            transform.pivot = newPivotPos;
+            return;
+        }
+
+        // This gives support to scaling, this is important when working with the pivot.
+        Vector2 pivotDelta  = newPivotPos - transform.pivot;
+        pivotDelta.x       /= transform.localScale.x; 
+        pivotDelta.y       /= transform.localScale.y; 
+        pivotDelta          = pivotDelta.FixNaN();
+
+        // Unity moves the rect to the pivot target position when moving the pivot with 
+        // code, also does strange things when the anchors are not joinded, so with this 
+        // we calculate ourselves the position that should have the rect to correct these issues.
+        Vector2 correctPosition = GetRectCoordinatesFromPivotPosition(transform, newPivotPos);
+        transform.pivot        += pivotDelta;
+        RteRectTools.SetPositionIgnoringAnchors(transform, correctPosition);
+    }
+
+    /// <summary>
+    /// Convert pivot coordinates (values between 0 and 1) into rect coordinates.
+    /// </summary>
+    public static Vector2 GetPivotPositionFromRectCoordinatesIgnoringAnchors(RectTransform tr, Vector2 rectCoordinates)
+    {
+        rectCoordinates -= RteRectTools.GetPositionIgnoringAnchorsAndPivot(tr);
+        return new Vector2(rectCoordinates.x / tr.rect.size.x, rectCoordinates.y / tr.rect.size.y).FixNaN();
+    }
+
+    /// <summary>
+    /// Convert rect coordinates into pivot coordinates (values between 0 and 1).
+    /// </summary>
+    public static Vector2 GetRectCoordinatesFromPivotPosition(RectTransform tr, Vector2 pivotPosition)
+    {
+        Vector2 pivotPosInLocalRectCoord = new Vector2(pivotPosition.x * tr.rect.size.x, pivotPosition.y * tr.rect.size.y);
+        Vector2 rectPos                  = RteRectTools.GetPositionIgnoringAnchorsAndPivot(tr);
+        return pivotPosInLocalRectCoord + rectPos;
+    }
+
+    public static Vector2 GetCanvasPositionFromLocalPosition(RectTransform tr, Vector2 pivotPosition)
+    {
+        Vector2 size     = RteRectTools.GetSizeIgnoringAnchors(tr);
+        Vector2 position = RteRectTools.GetPositionIgnoringAnchorsAndPivot(tr);
+        size.x  *= pivotPosition.x;
+        size.y  *= pivotPosition.y;
+        size    += position;
+        return RteAnchorTools.GetCanvasAnchorCoordinateNormalizedFromRectCoordinate(tr, size);
+    }
+
+    public static Vector2 GetLocalPositionFromCanvasPosition(RectTransform transform, Vector2 canvasPosition)
+    {
+        Vector2 rectCoordinate = RteAnchorTools.GetRectCoordinateFromCanvasAnchorCoordinateNormalized(transform, canvasPosition);
+        Vector2 position       = RteRectTools.GetPositionIgnoringAnchorsAndPivot(transform);
+        Vector2 size           = RteRectTools.GetSizeIgnoringAnchors(transform);
+        rectCoordinate        -= position;
+        rectCoordinate.x      /= size.x;
+        rectCoordinate.y      /= size.y;
+        rectCoordinate.FixNaN();
+        return rectCoordinate;
+    }
+}
